54ce3fff4f2e61a187a7cf7cbb5d8e2b5eaf987c
fan <fan@163.com>
2020-05-13 13:41:10 +0800

    首页布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="container-home">Home</div>
+  <el-container class="container-home">
+    <el-aside width="200px" class="my-aside">Aside</el-aside>
+      <el-container>
+        <el-header class="my-header">Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -9,4 +15,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.container-home{
+  width: 100%;
+  height:100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: blue;
+  .my-aside{
+    background-color: #002233;
+  }
+  .my-header{
+    border-bottom: 1px solid #ddd;
+  }
+}
 </style>

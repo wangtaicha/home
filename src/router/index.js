3bdb6de588a822f7de29fd204a0d81367a380fb6
fan <fan@163.com>
2020-05-13 15:56:58 +0800

    404页面

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,22 @@ import Vue from 'vue'
 // 一级路由
 import Login from '@/views/login' // 登录组件
 import Home from '@/views/home' // home组件
+import Welcome from '@/views/welcome' // Welcome组件 欢迎页面
+import Notfound from '@/views/404' // notFound组件 404页面
 Vue.use(VueRouter)
 // 初始化
 const router = new VueRouter({
   routes: [
     { path: '/login', component: Login }, // 登录
-    { path: '/', component: Home } // home
+    {
+      path: '/',
+      component: Home,
+      children: [
+        { path: '/', component: Welcome }
+      ]
+    }, // home
+    // 通配规则(以上所有的规则不符合的时候,会走这个规则)
+    { path: '*', component: Notfound }
   ]
 })
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,88 @@
 <template>
   <el-container class="container-home">
-    <el-aside width="200px" class="my-aside">Aside</el-aside>
+    <el-aside :width="isOpen?'200px':'64px'" class="my-aside">
+      <!-- logo区域 -->
+      <div class="logo" :class="{minLogo:!isOpen}"></div>
+      <!-- 导航菜单 -->
+      <el-menu
+      default-active="2"
+      background-color="#002233"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      style="border-right:none"
+      :collapse="!isOpen"
+      :collapse-transition="false">
+      <el-menu-item index="1">
+        <i class="el-icon-s-home"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <i class="el-icon-document"></i>
+        <span slot="title">内容管理</span>
+      </el-menu-item>
+         <el-menu-item index="3">
+        <i class="el-icon-picture"></i>
+        <span slot="title">素材管理</span>
+      </el-menu-item>
+         <el-menu-item index="4">
+        <i class="el-icon-s-promotion"></i>
+        <span slot="title">发布文章</span>
+      </el-menu-item>
+         <el-menu-item index="5">
+        <i class="el-icon-chat-dot-round"></i>
+        <span slot="title">评论管理</span>
+      </el-menu-item>
+         <el-menu-item index="6">
+        <i class="el-icon-present"></i>
+        <span slot="title">粉丝管理</span>
+      </el-menu-item>
+         <el-menu-item index="7">
+        <i class="el-icon-setting"></i>
+        <span slot="title">个人设置</span>
+      </el-menu-item>
+    </el-menu>
+    </el-aside>
       <el-container>
-        <el-header class="my-header">Header</el-header>
-        <el-main>Main</el-main>
+        <el-header class="my-header">
+          <span class="icon el-icon-s-fold" @click="toggleMenu"></span>
+          <span class="text">江苏传智播客科技教育有限公司</span>
+          <!-- 右边下拉菜单 -->
+          <el-dropdown class="my-dropdown">
+          <span class="el-dropdown-link">
+          <!-- 用户头像 -->
+          <img class="head" src="../../assets/avatar.jpg" alt="">
+          <!-- 用户名称 -->
+          <strong class="name">你家小琦哥哥</strong>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人设置</el-dropdown-item>
+            <el-dropdown-item>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        </el-header>
+        <el-main>
+          <!-- 二级路由显示位置 -->
+          <router-view></router-view>
+        </el-main>
       </el-container>
   </el-container>
 </template>
 
 <script>
 export default {
-  name: 'app-home'
+  name: 'app-home',
+  data () {
+    return {
+      isOpen: true
+    }
+  },
+  methods: {
+    toggleMenu () {
+      // 切换状态 展开状态,收起的状态
+      this.isOpen = !this.isOpen
+    }
+  }
 }
 </script>
 
@@ -21,12 +93,42 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  background-color: blue;
+  // background-color: blue;
   .my-aside{
     background-color: #002233;
+    .logo{
+      width: 100%;
+      height: 60px;
+      // background: ..... 前面是背景图定位/ 背景图尺寸
+      background: #002244 url(../../assets/logo_admin.png) no-repeat center / 140px auto;
+    }
+    .minLogo{
+      background: url(../../assets/logo_admin_01.png) no-repeat center / 36px auto;
+    }
   }
   .my-header{
     border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .icon{
+      font-size: 24px;
+      vertical-align: middle;
+    }
+    .text{
+      vertical-align: middle;
+      padding-left: 10px;
+    }
+    .my-dropdown{
+      float: right;
+      .head{
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .name{
+        padding-left: 5px;
+        vertical-align: middle;
+      }
+    }
   }
 }
 </style>
